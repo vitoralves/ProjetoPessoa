928b1426627e07ed1ba2e47bb1036db9b14ccb67
Leandro D'Souza <Leandro D'Souza@DESKTOP-914FQGF>
2020-06-14 22:17:38 -0300

    Criação do Projeto.

---
+++ src/main/java/com/br/pessoa/ProjetoPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.br.pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPessoaApplication.class, args);
+	}
+
+}
